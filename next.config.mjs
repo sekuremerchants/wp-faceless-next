d606a69ff63fc67ba3f7ffb8482d86b3b9f94124
sekuremerchants <marketing@sekuremerchants.com>
2026-01-15 16:32:08 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  //output: 'export', // <=== enables static exports
+  //basePath: '/sekuremerchants/wp-faceless-next',
   matcher: [
     "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.[^/]+$).*)"
   ],
