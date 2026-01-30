1ce78f9971c782dddb7063415671fb2ee3cc2187
sekuremerchants <marketing@sekuremerchants.com>
2026-01-30 14:04:59 -0500

    Create page.js

---
+++ app/blog/business-type/[slug]/page.js
@@ -0,0 +1,74 @@
+const query = `
+  query GetPostsByBusinessType($uri: String!) {
+    posts(first: 9, where: {taxQuery: {taxArray: {taxonomy: BUSINESSTYPE, field: SLUG, operator: IN, terms: [$uri,]}}}) {
+      nodes {
+        date
+        id
+        slug
+        title
+        businessTypes {
+          nodes {
+            name
+          }
+        }
+      }
+    }
+  }
+`;
+
+const businessTypesQuery = `
+  query GetBusinessTypes {
+    businessTypes {
+      nodes {
+        name
+        slug
+      }
+    }
+  }
+`;
+
+export async function generateStaticParams(){
+  const res = await fetch("https://wordpress-dev-appsvc.azurewebsites.net/graphql", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: businessTypesQuery,
+    }),
+  });
+  const { data } = await res.json();
+
+	return data.businessTypes.nodes.map((post) => ({
+    slug: post.slug,
+  }));
+}
+
+export default async function BlogPost({params}) {
+	const slug = await params;
+	const queryVariables = {
+  		uri: 'blog/business-type/' + slug.slug,
+	};
+  const res = await fetch("https://wordpress-dev-appsvc.azurewebsites.net/graphql", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: query,
+			variables: queryVariables,
+    }),
+  });
+  const { data } = await res.json();
+
+	return (
+		<main>
+			<h1>dynamic blog post business type archive file - {data.posts.nodes[0].businessTypes.nodes[0].name}</h1>
+      <ul>
+        {data.posts.nodes.map((post) => (
+          <li key={post.id}>{post.title} - {post.date}</li>
+        ))}
+      </ul>
+		</main>  
+	);
+}
