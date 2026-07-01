7b45d6d2bc621921c41da5f1ece7fdaa8ff7b70d
sekuremerchants <marketing@sekuremerchants.com>
2026-07-01 11:50:20 -0400

    Update page.js

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -185,7 +185,7 @@ export default async function BlogPost({params}) {
   const { data } = await res.json();
   const nodeData = data.nodeByUri;
 
-  console.log('POST DATA: ', nodeData)
+  //console.log('POST DATA: ', nodeData)
 
   const allPosts = await getAllPosts();
   const filterPostsForRelated = allPosts.map((post) => {
